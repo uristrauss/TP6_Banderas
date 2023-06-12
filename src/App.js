e0fb6f2763556353b24a0295fc397231b4708ee6
uristrauss <uriel.strauss@gmail.com>
2023-06-12 15:52:44 -0300

    Creacion del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
 
-function App() {
+export default function App() {
+  const [isLoading, setIsLoading] = useState (true);
+  const [flagImage ,  setFlagImage] = useState('');
+
+  useEffect(() =>{
+    fetch('https://countriesnow.space/api/v0.1/countries/flag/images')
+    .then ((response)=> response.json()) //que la respuesta sea en json
+    .then ((all)=> {
+      setFlagImage (all.data)
+      setIsLoading(false)
+    });
+  },[]);
+
+
+  if(isLoading){
+    return (
+      <div className = "App">
+        <h1>Cargando ...</h1>
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src = {flagImage} alt = "Una bandera" />
     </div>
   );
 }
 
-export default App;
