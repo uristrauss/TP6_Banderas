93eeed9aa0b199e6f66e4862426a61d7d8e115cf
uristrauss <uriel.strauss@gmail.com>
2023-07-06 10:33:53 -0300

    Se guarda en Pais

--- src/Banderas.js
+++ src/Banderas.js
@@ -1,58 +1,51 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+function darBandera(banderas) {
+  return banderas[Math.floor(Math.random() * banderas.length)];
+}
 
 function Banderas() {
-const [banderas, setBanderas] = useState([]);
-const [cargando, setCargando] = useState(true);
-// const [pais, setPais] = useState({});
+  const [banderas, setBanderas] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [pais, setPais] = useState({});
+  const [puntos, setPuntos] = useState(0);
 
-    useEffect(() =>{
-
-        axios.get('https://countriesnow.space/api/v0.1/countries/flag/images') // vamos a la API
-        .then ((response)=> {
-        //let numerito = numeroRandom();
-        //    console.log(response)
+  useEffect(() => {
+    axios
+      .get('https://countriesnow.space/api/v0.1/countries/flag/images')
+      .then((response) => {
         setBanderas(response.data.data);
         setCargando(false);
-        //setPais(numeroRandom());
-        })
-        .catch((error) => {
-            console.error(error);
-            setCargando(false);
-          });
-        }, []);
+        setPais(darBandera(response.data.data)); 
+      })
+      .catch((error) => {
+        console.error(error);
+        setCargando(false);
+      });
+  }, []);
 
+  if (cargando) {
+    return <h1>cargandooooooo</h1>;
+  } else {
+    {console.log(pais.name)}
+    return ( 
+      <div>
         
-        function numeroRandom(max) {
-        return Math.floor(Math.random()*banderas.length);
-            //return banderas[Math.floor(Math.random()*banderas.length)];
-        }
-
-      if (cargando) {
-        return (
-            <h1>cargandooooooo</h1>
-        )
-      }else {
-        return (
-        <img src={banderas[numeroRandom()].flag} alt="Una bandera" />
-        )
-      }
+      <img src={pais.flag} alt="Una bandera" />
+      <input className="form-control" type="text" placeholder="Default input" aria-label="default input example" onChange={(e) => Adivinar(e.target.value)}/>
+      <button type="submit" className="btn btn-primary">Submit</button>
       
-
-      /*
-       return (
-    <div className="App">
-      {cargando ? (
-        <span>Cargando...</span>
-      ) : (
-        <img src={banderas[numeroRandom()].flag} alt="Una bandera" />
-      )}
     </div>
-  );
-  */
+    
 
+    );
+    
+  }
+
+  function Adivinar(){
+
+  }
 }
 
 export default Banderas;
-
